e6d7485607b12935a460b19dd793940d29c8ced4
tomoyuki65 <tomoyuki65@users.noreply.github.com>
2022-11-13 03:24:03 +0900

    create rails app

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::API
+
+  # テスト用のAPIを追加
+  def test
+    # テスト用のJSON形式のオブジェクト
+    test_json_obj = [
+      { id: 1, title: "First Text", text: "最初のテキスト" },
+      { id: 2, title: "Second Text", text: "2番目のテキスト" },
+    ]
+
+    # JSON形式で出力
+    render json: test_json_obj
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  # テスト用のルーティング
+  scope "api" do
+    scope "v1" do
+      get "/test", to: "application#test"
+    end
+  end
+
+end
