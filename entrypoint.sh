ca63d3aeef56b0c421f61bfeeeaca6954429dbc3
tomoyuki65 <tomoyuki65@users.noreply.github.com>
2022-11-16 22:02:55 +0900

    modify database.yml & entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,12 @@ rm -f /spa2210-b/tmp/pids/server.pid
 if [ "$RAILS_ENV" = "production" ]; then
   bundle exec rails assets:clobber
   bundle exec rails assets:precompile
-  bundle exec rails db:migrate
+  # 本番環境（AWS ECS）への初回デプロイ時に利用
+  # 初回デプロイ後にコメントアウトして下さい
+  bundle exec rails db:create
+  # 2回目以降のデプロイ時に利用
+  # 初回デプロイ後にコメントアウトを外して下さい
+  # bundle exec rails db:migrate
 fi
 
 # サーバー実行(DockerfileのCMDをセット)
